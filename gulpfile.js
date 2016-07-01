20f288cc5345055469366d4f03013d3392b3de2f
Katya <katyaskvo@gmail.com>
2016-07-01 13:20:05 -0700

    mobile first layout

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	maps = require('gulp-sourcemaps'),
+	livereload = require('gulp-livereload'),
+	del	 = require('del');
+
+gulp.task('compileSass', function(){
+	return gulp.src('scss/main.scss')
+	.pipe(maps.init())
+	.pipe(sass())
+	.pipe(maps.write('./'))
+	.pipe(gulp.dest('css'))
+	.pipe(livereload());
+});
+
+gulp.task('clean', function(){
+	del(['dist', 'css/*']);
+});
+
+gulp.task('watchFiles', function(){
+	livereload.listen();
+	gulp.watch('scss/*.scss', ['compileSass']);
+})
+gulp.task('serve', ['watchFiles']);
+
+gulp.task('build', ['compileSass'], function(){
+	return gulp.src(['css/main.css', 'index.html', 'img/**', 'fonts/**'], { base: './' })
+	.pipe(gulp.dest('dist'));
+});
+gulp.task('default', ['clean'], function(){
+	gulp.start('build');
+});
