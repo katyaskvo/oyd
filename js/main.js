4d8107f071ca216b2c3fe1783a6ee48ea250beb6
Katya <katyaskvo@gmail.com>
2016-07-11 13:24:28 -0700

    add animation effects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 $('.svg-inject').svgInject();
+
+jQuery(document).ready(function() {
+	jQuery('.oyd-logo').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated bounceInDown',
+		offset: 0
+    });	
+    
+    jQuery('.now-available').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated bounceInLeft',
+		offset: 0
+	});	
+
+	jQuery('.animate-fadein').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated fadeIn',
+		offset: 0
+    });
+    
+    jQuery('.dead-giblet').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated bounceInDown',
+		offset: -50
+    });
+       
+});
