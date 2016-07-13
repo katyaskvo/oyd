2855b5f97950edab7ac53714934397a941243f14
Katya <katyaskvo@gmail.com>
2016-07-13 15:13:01 -0700

    set max width for youtube video

---
+++ dist 2/js/main.js
@@ -0,0 +1,24 @@
+$('.svg-inject').svgInject();
+
+jQuery(document).ready(function() {
+	jQuery('.oyd-logo').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated bounceInDown',
+		offset: 0
+    });	
+    
+    jQuery('.now-available').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated bounceInLeft',
+		offset: 0
+	});	
+
+	jQuery('.animate-fadein').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated fadeIn',
+		offset: -30
+    });
+    
+    jQuery('.dead-giblet').addClass("hidden").viewportChecker({
+		classToAdd: 'visible animated bounceInDown',
+		offset: -50
+    });
+       
+});
